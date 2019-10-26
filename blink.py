37fc9da8c67e782dce09a5f79a3ff244b4288fbf
Kevin McAleer <kevinmcaleer@me.com>
2019-10-26 20:08:20 +0100

    initial commit

---
+++ blink.py
@@ -0,0 +1,14 @@
+from pyfirmata import Arduino, util
+import time
+
+board = Arduino("COM3")
+
+loopTimes = input('How many times would you like the LED to blink: ')
+print("Blinking " + loopTimes + " times.")
+
+for x in range(int(loopTimes)):
+    board.digital[13].write(1)
+    time.sleep(0.2)
+    board.digital[13].write(0)
+    time.sleep(0.2)
+     
